1d99782c29d6ba47efffecad6db5c7c9cfafb6bd
KumarArnob <kumararnob07@gmail.com>
2021-01-18 21:32:07 +0530

    connection check 3

--- webdev2/settings.py
+++ webdev2/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os 
 from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ SECRET_KEY = 'k)myi10-&*u)_!sghnelpvh-m@2z-m9%#u!72mb9&-(b7i+cwy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['webtel0.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -119,7 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
